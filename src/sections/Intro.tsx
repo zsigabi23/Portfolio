2bd37d437423ee7c163840df5c8b424899ddab7c
Zolile Sigabi <zsigabi@student.wethinkcode.co.za>
2025-01-07 14:31:48 +0200

    Intro Section

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,7 +1,36 @@
-import { FC } from "react";
+"use client";
+
+import { stagger, useAnimate, useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
-  return <section className="section lg:mt-20 md:mt-16 mt-12" id="intro">
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, { once: true });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span"
+    });
+  },[scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(
+        scope.current.querySelectorAll(".word"),
+        {
+          transform: "translateY(0%)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [inView, scope, animate]);
+
+  return <section className="section lg:mt-20 md:mt-16 mt-12" id="intro" ref={scope}>
     <div className="container">
       <h2 className="text-4xl md:text-7xl lg:w-[80%] lg:text-8xl">Building beautiful websites with clean code and thoughtful design to
         help your business grow and stand out from the competition.
